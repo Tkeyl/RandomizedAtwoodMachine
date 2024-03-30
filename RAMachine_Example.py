1c5a614a9d6e6b9a549c49f31e0fa0d4f27c9fcf
Tkeyl <158479362+Tkeyl@users.noreply.github.com>
2024-03-30 14:45:32 -0500

    Add files via upload

---
+++ RAMachine_Example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from RAMachine_FinalVersion import *
+
+
+chosen_vars, randomized_vars, solved_for_vars = RAMachine()
+
+
+####### printing out returned dictionary work #########################
+print(f'chosen_vars = {chosen_vars}')
+print(f'randomized_vars = {randomized_vars}')
+print(f'solved_for_vars = {solved_for_vars}')
+print('\n')
+
+####### grabbing a dictionary entry and assigning it to a variable ####
+randomized_v0 = randomized_vars[rand_v0]
+print(f'randomized_v0 = {randomized_v0}')
